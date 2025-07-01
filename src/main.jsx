251c393667e81afae405bc435e2310b9960a83ba
MelePdem <melesedemessie88@gmail.com>
2025-07-01 14:58:08 -0400

    fix :added hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 import "./assets/css/styles.css";
 import "./assets/css/bootstrap.css";
- import {BrowserRouter} from "react-router-dom"
+ import {HashRouter} from "react-router-dom"
 // import "./assets/js/bootstrap.js"
 import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter >
+   <HashRouter >
     <App />
-    </BrowserRouter >
+</HashRouter>
+   
   </StrictMode>
 );
