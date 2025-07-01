b09c4a482eec51d769e3626c7798c0672f2ca2c3
MelePdem <melesedemessie88@gmail.com>
2025-07-01 15:09:52 -0400

    remove base path for vercle

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/apple-clone-project/", 
-  plugins: [react()],
+   plugins: [react()],
   build: {
     sourcemap: false,
   },
